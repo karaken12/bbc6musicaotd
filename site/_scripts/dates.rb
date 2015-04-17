77e6bd5af472b106605b22500df8824218f71fb2
Tom Potts <karaken12@gmail.com>
2015-04-17 11:31:25 +0100

    De-duplicating by date.

--- site/_scripts/dates.rb
+++ site/_scripts/dates.rb
@@ -16,9 +16,17 @@ def update_file(file_name)
 end
 
 def process_data(data)
+  dates = {}
+  dates.default = 0
   data.each do |album|
     if !album.has_key?('date') then next end
     album['date'] = Date.parse(album['date'].to_s)
+    dates[album['date']] += 1
+  end
+  dates.each do |date, instances|
+    if instances > 1
+      puts "#{date} has #{instances} occurances."
+    end
   end
   return data
 end

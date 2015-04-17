6838933caa24bdae06b54e71290fa91be994d255
Tom Potts <karaken12@gmail.com>
2015-04-17 11:33:12 +0100

    Build script update.

--- site/build.sh
+++ site/build.sh
@@ -1,4 +1,12 @@
 
 ruby _scripts/twitpic.rb _data/2015.yml
+ruby _scripts/twitpic.rb _data/2014.yml
+ruby _scripts/twitpic.rb _data/2013.yml
+ruby _scripts/twitpic.rb _data/2012.yml
+ruby _scripts/twitpic.rb _data/2011.yml
 ruby _scripts/spotify_search.rb _data/2015.yml
+ruby _scripts/spotify_search.rb _data/2014.yml
+ruby _scripts/spotify_search.rb _data/2013.yml
+ruby _scripts/spotify_search.rb _data/2012.yml
+ruby _scripts/spotify_search.rb _data/2011.yml
 jekyll build

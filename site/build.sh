3a887ac06df161189f27180ba2202673772ac4c8
Tom Potts <karaken12@gmail.com>
2015-04-17 15:10:41 +0100

    Simplified build process.

--- site/build.sh
+++ site/build.sh
@@ -1,12 +1,7 @@
 
-ruby _scripts/twitpic.rb _data/2015.yml
-ruby _scripts/twitpic.rb _data/2014.yml
-ruby _scripts/twitpic.rb _data/2013.yml
-ruby _scripts/twitpic.rb _data/2012.yml
-ruby _scripts/twitpic.rb _data/2011.yml
-ruby _scripts/spotify_search.rb _data/2015.yml
-ruby _scripts/spotify_search.rb _data/2014.yml
-ruby _scripts/spotify_search.rb _data/2013.yml
-ruby _scripts/spotify_search.rb _data/2012.yml
-ruby _scripts/spotify_search.rb _data/2011.yml
+./year-build.sh _data/2011.yml
+./year-build.sh _data/2012.yml
+./year-build.sh _data/2013.yml
+./year-build.sh _data/2014.yml
+./year-build.sh _data/2015.yml
 jekyll build
---
+++ site/year-build.sh
@@ -0,0 +1,3 @@
+ruby _scripts/twitpic.rb $1
+ruby _scripts/spotify_search.rb $1
+ruby _scripts/spotify_cover.rb $1

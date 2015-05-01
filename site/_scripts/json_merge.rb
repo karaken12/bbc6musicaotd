f0d25fe1770b51363eb045e6c40e82d11a320c33
Tom Potts <karaken12@gmail.com>
2015-05-01 11:30:21 +0100

    Combined all JSON data into YAML.

--- site/_scripts/json_merge.rb
+++ site/_scripts/json_merge.rb
@@ -6,12 +6,12 @@ require 'uri'
 #config_path = File.expand_path('app_secret.yml', File.dirname(__FILE__))
 #$app_config = YAML.load_file(config_path)
 
-def copy_file(file_name, new_file_name)
+def copy_file(file_name, existing_data)
   data = YAML.load_file(file_name)
 
   new_data = process_data(data)
 
-  puts new_data.to_yaml
+  existing_data.concat(new_data)
 
   #file = File.open(new_file_name, 'w')
   #file.puts new_data.to_yaml
@@ -21,6 +21,10 @@ end
 def process_data(data)
   new_data = []
   data.each do |album|
+    # Actually just going to ignore everything with no date.
+    if (album['date'] == '')
+      next
+    end
     new_album = {}
     new_album['date'] = Date.parse(album['date'])
     new_album['title'] = album['title'].strip()
@@ -44,8 +48,33 @@ def process_data(data)
   return new_data
 end
 
-# Bit nasty, but should do the job
-if (ARGV[0] and ARGV[1])
-  copy_file(ARGV[0], ARGV[1])
+def dedup(data)
+  new_data = []
+  for album in data
+    if new_data.find_index(album) == nil
+      new_data.push(album)
+    elsif new_data.find_index{|a| a != album and a['date'] == album['date']} != nil
+      puts "Duplicate date: #{album['date']}!"
+      index = new_data.find_index{|a| a['date'] == album['date']}
+      puts "Overall: #{new_data[index] == album}"
+      puts "Dates: #{new_data[index]['date'] == album['date']}"
+      puts "Titles: #{new_data[index]['title'] == album['title']}"
+      puts "Artists: #{new_data[index]['artist'] == album['artist']}"
+      puts "Reviews: #{new_data[index]['review'] == album['review']}"
+      new_data.push(album)
+    end
+  end
+  return new_data
 end
 
+# Bit nasty, but should do the job
+data = []
+for arg in ARGV
+  copy_file(arg, data)
+end
+
+puts data.size
+data = dedup(data)
+puts data.size
+
+puts data.to_yaml

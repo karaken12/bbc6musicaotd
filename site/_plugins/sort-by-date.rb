3f9bb74bd2bfb920795129075718972df3e08bc1
Tom Potts <karaken12@gmail.com>
2015-05-05 10:12:17 +0100

    Moving year data files to subfolder of _data.

--- site/_plugins/sort-by-date.rb
+++ site/_plugins/sort-by-date.rb
@@ -1,8 +1,9 @@
 module JekyllBbc6musicaotd
   class SortByDate < Jekyll::Generator
     def generate(site)
-      site.data['2014'].sort!{|a,b| a['date'] <=> b['date']}
-      site.data['2015'].sort!{|a,b| a['date'] <=> b['date']}
+      site.data['year'].values.each do |year|
+        year.sort!{|a,b| a['date'] <=> b['date']}
+      end
     end
   end
 end
--- site/build.sh
+++ site/build.sh
@@ -1,8 +1,6 @@
 
-./year-build.sh _data/2010.yml
-./year-build.sh _data/2011.yml
-./year-build.sh _data/2012.yml
-./year-build.sh _data/2013.yml
-./year-build.sh _data/2014.yml
-./year-build.sh _data/2015.yml
+for f in _data/year/*.yml
+do
+  ./year-build.sh $f
+done
 jekyll build

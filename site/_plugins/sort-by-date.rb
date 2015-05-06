1b8cba1731d311f7f51b5d5b557b96f08281b4f9
Tom Potts <karaken12@gmail.com>
2015-05-06 11:41:16 +0100

    Moving album layout to an include file.

--- site/_plugins/sort-by-date.rb
+++ site/_plugins/sort-by-date.rb
@@ -4,6 +4,9 @@ module JekyllBbc6musicaotd
       site.data['year'].values.each do |year|
         year.sort!{|a,b| a['date'] <=> b['date']}
       end
+      this_year = '2015'
+      this_year_albums = site.data['year'][this_year]
+      site.data['recent'] = this_year_albums.reverse[0,5]
     end
   end
 end

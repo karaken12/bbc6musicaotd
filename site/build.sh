a9828efc6f3aa55512473f2dce098a391e7d0c7d
Tom Potts <karaken12@gmail.com>
2015-05-07 15:40:19 +0100

    Move year build script into main build script, now we can build just one year at a time anyway.

--- site/build.sh
+++ site/build.sh
@@ -1,6 +1,10 @@
 
 for f in _data/year/*${1}.yml
 do
-  ./year-build.sh $f
+  echo " === Building $f === "
+  ruby _scripts/twitpic.rb $f
+  ruby _scripts/spotify_search.rb $f
+  ruby _scripts/spotify_cover.rb $f
+  ruby _scripts/reorder.rb $f
 done
 jekyll build
--- site/year-build.sh
+++
@@ -1,5 +0,0 @@
-echo " === Building $1 === "
-ruby _scripts/twitpic.rb $1
-ruby _scripts/spotify_search.rb $1
-ruby _scripts/spotify_cover.rb $1
-ruby _scripts/reorder.rb $1

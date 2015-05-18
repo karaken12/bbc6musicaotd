73f7180a4a8a6443bdf6bffa2f151338cd70a311
Tom Potts <karaken12@gmail.com>
2015-05-18 09:41:12 +0100

    Adding week separators to the year templates.

--- site/_plugins/sort-by-date.rb
+++ site/_plugins/sort-by-date.rb
@@ -1,8 +1,16 @@
 module JekyllBbc6musicaotd
   class SortByDate < Jekyll::Generator
+    def monday(date)
+      # Modulus idea from Phrogz on SO; not really nescessary but keeps us safe.
+      date - (date.wday - 1)%7
+    end
+
     def generate(site)
       site.data['year'].values.each do |year|
         year.sort!{|a,b| a['date'] <=> b['date']}
+        year.each do |album|
+          album['week'] = week_start(album['date'],1)
+        end
       end
       this_year = '2015'
       this_year_albums = site.data['year'][this_year]

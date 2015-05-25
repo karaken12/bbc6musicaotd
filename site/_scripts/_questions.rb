9b4ed96879f5611c4b4e264e37a51532145c9042
Tom Potts <karaken12@gmail.com>
2015-05-25 23:23:15 +0100

    Move Spotify questions to question bank.

--- site/_scripts/_questions.rb
+++ site/_scripts/_questions.rb
@@ -15,3 +15,34 @@ def ask_for_twitter_data(tweet, album)
   return return_data
 end
 
+def choose_candidates(candidates, selected_id)
+  puts "#{candidates.size} candidates:"
+  default = ""
+  candidates.each_with_index do |candidate, index|
+    title = candidate['name']
+    type = candidate['type']
+    id = candidate['album_id']
+    artist = candidate['artists'].join(', ')
+    if id == selected_id
+      ast = "*"
+      default = index
+    else
+      ast = " "
+    end
+    puts "#{ast}#{index}: #{title} by #{artist} (#{type}, #{id})"
+  end
+  print "Enter option (#{default}): "
+  chosen = STDIN.gets.chomp
+  if chosen == nil or chosen.strip() == ''
+    chosen = default
+  end
+  if chosen == nil or chosen.strip() == ''
+    return nil
+  end
+  begin
+    chosen_index = Integer(chosen)
+  rescue ArgumentError
+    return nil
+  end
+  return chosen_index
+end
--- site/_scripts/spotify_walker.rb
+++ site/_scripts/spotify_walker.rb
@@ -1,5 +1,6 @@
 
 require 'yaml'
+require_relative '_questions'
 
 def update_file(file_name)
   data = YAML.load_file(file_name)
@@ -28,7 +29,6 @@ def process_data(data)
     end
     candidates = album['spotify']['candidates']
     selected_id = nil
-    default = ""
     if album['spotify'].has_key?('selected')
       selected_id = album['selected']['album_id']
     end
@@ -36,33 +36,10 @@ def process_data(data)
     if album.has_key?('notes')
       puts "(#{album['notes']})"
     end
-    puts "#{candidates.size} candidates:"
-    candidates.each_with_index do |candidate, index|
-      title = candidate['name']
-      type = candidate['type']
-      id = candidate['album_id']
-      artist = candidate['artists'].join(', ')
-      if id == selected_id
-        ast = "*"
-        default = index
-      else
-        ast = " "
-      end
-      puts "#{ast}#{index}: #{title} by #{artist} (#{type}, #{id})"
-    end
-    print "Enter option (#{default}): "
-    chosen = STDIN.gets.chomp
-    if chosen == nil or chosen.strip() == ''
-      chosen = default
-    end
-    if chosen == nil or chosen.strip() == ''
-      next
-    end
-    begin
-      chosen_index = Integer(chosen)
-    rescue ArgumentError
-      next
-    end
+
+    chosen_index = choose_candidates(candidates, selected_id)
+    if chosen_index == nil then next end
+    
     album['spotify-id'] = candidates[chosen_index]['album_id']
     album.delete('spotify')
     #print "Enter artist (#{album['artist']}): "

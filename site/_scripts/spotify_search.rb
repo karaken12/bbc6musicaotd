a84569e7ea7a7d81762c4b01e3b61b5916a1ea8c
Tom Potts <karaken12@gmail.com>
2015-07-30 14:28:07 +0100

    Move Spotify candidate data to a separate file.

--- site/_scripts/spotify_search.rb
+++ site/_scripts/spotify_search.rb
@@ -2,17 +2,27 @@
 require 'yaml'
 require_relative 'SpotifySearch'
 
+CANDIDATE_FILE_NAME = "_data/cache/spotify_candidates.yml"
+
 def update_file(file_name)
   data = YAML.load_file(file_name)
 
-  process_data(data)
+  candidates = process_data(data)
 
-  file = File.open(file_name, 'w')
-  file.puts data.to_yaml
+  candidate_data = YAML.load_file(CANDIDATE_FILE_NAME)
+  if !candidate_data || candidate_data == ''
+    candidate_data = {}
+  end
+  candidate_data[file_name] = candidates
+
+  file = File.open(CANDIDATE_FILE_NAME, 'w')
+  file.puts candidate_data.to_yaml
   file.close
 end
 
 def process_data(data)
+  candidate_data = []
+
   data.each do |album|
     if album.has_key?('spotify-id') then next end
     if (album['spotify'] and !(album['spotify']['candidates'])) then next end
@@ -28,8 +38,11 @@ def process_data(data)
     spotify_data = SpotifySearch.get_spotify_data(album['title'], album['artist'], album['type'])
     if spotify_data
       album['spotify'] = spotify_data
+      candidate_data.push(album)
     end
   end
+
+  return candidate_data
 end
 
 # Bit nasty, but should do the job
